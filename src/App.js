8f4ff6a4e72358e808a6978d62b5a343bb3ca394
arditsaliji <arditsaliji@live.de>
2019-12-12 21:16:37 +0100

    Introducing navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Login } from "./components/login/index";
+import { Login } from "./components/login";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Map from "./components/login/map";
 
 function App() {
-  return (
-    <div className="App">
-      <Login/>
-
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <div className="App">
+                    <Route path="/" exact component={Login}/>
+                    <Route path="/map" exact component={Map}/>
+                </div>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/login/map.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Map() {
+    return(
+        <div className="App">
+            <h1>
+                Map & List
+            </h1>
+        </div>
+    );
+
+}
+export default Map;
