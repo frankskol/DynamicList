3e5d4ee72568ac85fe289aa31f034e77febb059c
frank <francisco.litvay@protonmail.com>
2019-12-12 22:47:45 +0100

    Added initial Maps function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Login } from "./components/login";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Map from "./components/login/map";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <div className="App">
+                    <Route path="/" exact component={Login}/>
+                    <Route path="/map" exact component={Map}/>
+                </div>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
